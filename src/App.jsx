ee0cbd20d748c738a4fb3ec31a0f527c90271002
Rza Ismayilov <rzaismail82@gmail.com>
2022-05-15 12:07:13 +0400

    structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Login from "./components/auth/Login";
 import ContextController from "./context/ContextController";
+import Login from "./components/auth/Login";
+import Home from "./components/Home";
+import Error from "./components/Error";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <ContextController>
-      <div className="App">
-        <Login />
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Router>
     </ContextController>
   );
 }
