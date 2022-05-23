eaf5b65d5fa28fce6e6dec208d570d7adf534965
Rza Ismayilov <rzaismail82@gmail.com>
2022-05-23 20:21:36 +0400

    student API controller done

--- src/api/StudentAPI.js
+++ src/api/StudentAPI.js
@@ -62,3 +62,27 @@ export const DeleteStudentAPI = async (id, token) => {
 
   return result
 }
+
+export const UpdateStudentAPI = async (details, token) => {
+
+  let result = null
+
+  await Axios.put(STUDENT_URL + '/' + details.id, {
+    email: details.email,
+    password: details.password,
+    firstName: details.firstName,
+    lastName: details.lastName,
+    groupId: details.groupId,
+    admissionYear: details.admissionYear
+  }, {
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  }).then((response) => {
+    result = response.data
+  }).catch((error) => {
+    console.log(error)
+  })
+
+  return result
+}
