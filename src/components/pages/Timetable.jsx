89cf476d1feb23be7a93913afb31017953c578c4
Gaya <qayaasgerov@gmail.com>
2022-05-26 11:23:56 +0400

    timetable page edited

--- src/components/pages/Timetable.jsx
+++ src/components/pages/Timetable.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect }  from 'react';
+import { set } from 'date-fns';
+import { useState, useEffect } from 'react';
 import { GetLessonsOfStudentAPI } from '../../api/LessonAPI';
 import { useUserContext } from '../../context/UserContext';
+import NavbarClient from '../NavbarClient';
 
 const periods = [0, 1, 2, 3, 4]
 
@@ -25,6 +27,7 @@ function Timetable() {
     })
   }
 
+
   useEffect(() => {
     getLessons()
     console.log("HI")
@@ -35,76 +38,81 @@ function Timetable() {
   }, [week, lessons])
 
   return (
-    <div>
-      <button onClick={() => setWeek(week - 1)}>{'<---'}</button>
-      {week}
-      <button onClick={() => setWeek(week + 1)}>{'--->'}</button>
-      <table>
-        <thead>
-          <tr>
-            <th>day \ hour</th>
-            <th>08:30 - 10:00</th>
-            <th>10:15 - 11:45</th>
-            <th>12:45 - 14:15</th>
-            <th>14:30 - 16:00</th>
-            <th>16:15 - 17:45</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Mon</th>
-            {periods.map(p =>
-              <LessonBox key={p} lesson={
-                weekLessons.filter(lesson => lesson.day === 1).find(lesson => lesson.period === p)
-              }/>
-            )}
-          </tr>
-          <tr>
-            <th>Tue</th>
-            {periods.map(p =>
-              <LessonBox key={p} lesson={
-                weekLessons.filter(lesson => lesson.day === 2).find(lesson => lesson.period === p)
-              }/>
-            )}
-          </tr>
-          <tr>
-            <th>Wed</th>
-            {periods.map(p =>
-              <LessonBox key={p} lesson={
-                weekLessons.filter(lesson => lesson.day === 3).find(lesson => lesson.period === p)
-              }/>
-            )}
-          </tr>
-          <tr>
-            <th>Thu</th>
-            {periods.map(p =>
-              <LessonBox key={p} lesson={
-                weekLessons.filter(lesson => lesson.day === 4).find(lesson => lesson.period === p)
-              }/>
-            )}
-          </tr>
-          <tr>
-            <th>Fri</th>
-            {periods.map(p =>
-              <LessonBox key={p} lesson={
-                weekLessons.filter(lesson => lesson.day === 5).find(lesson => lesson.period === p)
-              }/>
-            )}
-          </tr>
-        </tbody>
-      </table>
+    <div className='timetable'>
+      <NavbarClient />
+      <div className="week-btns">
+        <button onClick={() => setWeek(week - 1)}><i className="fa-solid fa-arrow-left"></i></button>
+        <button onClick={() => setWeek(week + 1)}><i className="fa-solid fa-arrow-right"></i></button>
+      </div>
+      <h2>{user.firstName + ' ' + user.lastName + ' - Week ' + week}</h2>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Day \ Hour</th>
+              <th>08:30 - 10:00</th>
+              <th>10:15 - 11:45</th>
+              <th>12:45 - 14:15</th>
+              <th>14:30 - 16:00</th>
+              <th>16:15 - 17:45</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th title="Monday">Mon</th>
+              {periods.map(p =>
+                <LessonBox key={p} lesson={
+                  weekLessons.filter(lesson => lesson.day === 1).find(lesson => lesson.period === p)
+                } />
+              )}
+            </tr>
+            <tr>
+              <th title="Tuesday">Tue</th>
+              {periods.map(p =>
+                <LessonBox key={p} lesson={
+                  weekLessons.filter(lesson => lesson.day === 2).find(lesson => lesson.period === p)
+                } />
+              )}
+            </tr>
+            <tr>
+              <th title='Wednesday'>Wed</th>
+              {periods.map(p =>
+                <LessonBox key={p} lesson={
+                  weekLessons.filter(lesson => lesson.day === 3).find(lesson => lesson.period === p)
+                } />
+              )}
+            </tr>
+            <tr>
+              <th title='Thursday'>Thu</th>
+              {periods.map(p =>
+                <LessonBox key={p} lesson={
+                  weekLessons.filter(lesson => lesson.day === 4).find(lesson => lesson.period === p)
+                } />
+              )}
+            </tr>
+            <tr>
+              <th title='Friday'>Fri</th>
+              {periods.map(p =>
+                <LessonBox key={p} lesson={
+                  weekLessons.filter(lesson => lesson.day === 5).find(lesson => lesson.period === p)
+                } />
+              )}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
 
-function LessonBox({lesson}) {
-  return (
-    <td> {lesson === undefined ? "HELLO" :
-      lesson.group.name +
-      lesson.room +
-      lesson.teacher.firstName + lesson.teacher.lastName +
-      lesson.subject.name
-    } </td>
+function LessonBox({ lesson }) {
+  return (lesson === undefined ? <td></td> :
+    <td title={`${lesson.group.name}\n${lesson.room}\n${lesson.teacher.firstName} ${lesson.teacher.lastName}`}>
+      {/* // lesson.group.name +
+      // lesson.room +
+      // lesson.teacher.firstName + lesson.teacher.lastName + */}
+      {lesson.subject.name}
+    </td>
   )
 }
 
